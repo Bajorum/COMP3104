a3cf05552e962223b417c4d910c8d00fe6054150
Bajorum <Bilal@Youssef.ca>
2024-10-06 17:23:13 -0400

    Added React app with index.js and index.html

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to COMP3104!</h1>
+      <h2>Student Name: Bilal</h2>
+      <h2>Student ID: 101462289</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // You can create this CSS file for styling.
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
